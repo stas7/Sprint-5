58b632221a4bfc43a2dc6796b20374aa51dc8e95
Stanislav Naumenko <Naumenko.s.vik@sberbank.ru>
2021-10-16 18:47:31 +0300

    выполнил домашку с задержкой

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
@@ -1,6 +1,9 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -14,6 +17,8 @@ class JsonCustomNamingPropertyStrategyTest {
         val data =
             """{"FIRSTNAME": "Иван", "LASTNAME": "Иванов", "MIDDLENAME": "Иванович", "PASSPORTNUMBER": "123456", "PASSPORTSERIAL": "1234", "BIRTHDATE": "1990-01-01"}"""
         val objectMapper = ObjectMapper()
+            .registerModules(KotlinModule(), JavaTimeModule())
+            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
 
         // when
         val client = objectMapper.readValue<Client1>(data)
